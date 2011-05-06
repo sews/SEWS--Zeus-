bb5e6e3cc219d009e0cddf7f32c61dd70e1cd342
Per Wiberg <pewi6781@beurling.it.uu.se>
2011-05-06 12:12:01 +0200

    html_gen,lite get

--- src/sewsparser.erl
+++ src/sewsparser.erl
@@ -1,7 +1,17 @@
+
+%% @doc Sews Parser: Parses the reqests to the server to a format suitable to erlang.  
+
 -module(sewsparser).
 -export([parse/1]).
 
 
+%% For testing purposes only
+string()->
+    "GET /favicon.ico HTTP/1.1\r\nHost: 127.0.0.1:8888\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:2.0.1) Gecko/20100101 Firefox/4.0.1\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-us,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\nAccept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7\r\nKeep-Alive: 115\r\nConnection: keep-alive\r\n\r\n".
+
+
+
+%%%%%%%%%%%%%% EXPORTED FUNCTIONS %%%%%%%%%%%%%%
 %% @hidden@doc Parser for the html requests.
 %% @hidden@spec parse(Input::String) -> Tuple_list
 %%  Pre: A POST or GET request in the standard format
@@ -12,16 +22,18 @@
 
 %% TODO?: Impliment more calls then GET or POST?
 
-string()->
-    "GET /favicon.ico HTTP/1.1\r\nHost: 127.0.0.1:8888\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:2.0.1) Gecko/20100101 Firefox/4.0.1\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-us,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\nAccept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7\r\nKeep-Alive: 115\r\nConnection: keep-alive\r\n\r\n".
-
 parse(Input)->
-    [H|T] = string:tokens(Input," \n\r"),
-    case H of
-        "GET" -> parseGET(T,[]);
-        "POST" -> parsePOST(T,[]);
+    L = string:tokens(Input,"\n"),
+    [H|T] = lists:map(fun(X) -> string:tokens(X," \r\007") end,L),
+    [H1|Rest] = H,
+    case H1 of
+        "GET" -> parseGET([Rest|T],[]);
+        "POST" -> parsePOST([Rest|T],[]);
         Any -> {error, notGETorPOST}
     end.
+
+
+%%%%%%%%%%%%%% INTERNAL FUNCTIONS %%%%%%%%%%%%%%
 
 %% @doc Parse out the keywords and the relevant data from the keywords
 %% @spec parseGET(String::list,Tuple::list) -> Tuple::list
@@ -29,22 +41,34 @@ parse(Input)->
 %% Post: A tupple of the format {get, Tuple_list} or {error, Reason}, where Tuple_list consists of tuples of the format {header_atom(), Data}
 %% S-E None
 %% TODO: Implementera för fler headers 
-parseGET([],Parsed_list) ->
+parseGET([[]],Parsed_list) ->
     {get, Parsed_list};
-parseGET([H|T], []) ->
+parseGET([],Parsed_list)->
+    {get, Parsed_list};
+parseGET([[H|T2]|T], []) ->
     case H of
         [$/|Rest]-> parseGET(T,[{path,H}]);
         Any -> {error, badly_formed} % Kan vara badly formed även om den inte går in här såklart
     end;
-parseGET([H|T],Parsed_list) ->
+parseGET([[H|T2]|T],Parsed_list) ->
 	    case H of
 		"Host:" ->
-		    [Host|Rest] = T,
-		    parseGET(Rest,[{host, Host}|Parsed_list]);
+		    [Host|Rest] = T2,
+		    parseGET(T,[{host, Host}|Parsed_list]);
 		"Connection:" ->
-		    [Connection_type|Rest] = T,
-		    parseGET(Rest,[{connection,Connection_type}|Parsed_list]);
-		Any -> parseGET(T, Parsed_list)
+		    [Connection_type|Rest] = T2,
+		    parseGET(T,[{connection,Connection_type}|Parsed_list]);  %% kanske ta med mer info
+		"Accept-Language:" ->
+		    parseGET(T,[{accept_language,T2}|Parsed_list]);
+		"User-Agent:" -> 
+		    parseGET(T,[{user_agent,T2}|Parsed_list]);
+		"Accept-Encoding:" ->
+		    parseGET(T,[{accept_encoding,T2}|Parsed_list]);
+		"Accept-Charset:" ->
+		    parseGET(T,[{accept_charset,T2}|Parsed_list]);
+		"Keep-Alive:" ->
+		    parseGET(T,[{keep_alive,T2}|Parsed_list]);
+		Any -> parseGET(T, Parsed_list) %%% Impliment more later
     end.
 
 
