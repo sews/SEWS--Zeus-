705ebb569957e375d3aee928d6b2aa6c71b5773d
Johannes Henriksson <johe7425@svedberg.it.uu.se>
2011-05-10 11:40:38 +0200

    update

--- src/gen_html.erl
+++ src/gen_html.erl
@@ -15,30 +15,18 @@ dirDoc(DirList, HList)->
 	       false ->
 		   error_mod:handler(nohost)
 	   end,
-    DirFileList = fm:filesAndDirs(DirList,Path),
-    OrderingFun = fun({Atom1,File1},{Atom2,File2})-> 
-		      File1Low = string:to_lower(File1),
-		      File2Low = string:to_lower(File2),
-		      if 
-			  Atom2 == isdir, Atom1 == isdir, File1Low > File2Low -> false;
-			  Atom2 == isfile,Atom1 == isfile, File1Low > File2Low -> false;
-			  Atom2 == isdir,Atom1 == isfile -> false;
-			  true -> true
-		      end
-		  end,
-    SortedDirList = lists:sort(OrderingFun,DirFileList),
-    dirDocAux(SortedDirList, Path, Host,[]).
+    dirDocAux(DirList, Path, Host,[]).
 
 %% Todo: Make title show the path, optimize, fix the links so they work properly and sort them first according to type(directories/files) and then in lexographical order
 dirDocAux(DirList, Path, Host, [])->
     dirDocAux(DirList, Path, Host, "<HTML><HEAD><Title></title></HEAD><BODY>");
-dirDocAux([{Atom,File}|FileTail], Path, Host, Html) ->
-    case Atom of
-	 isdir -> %% File refers to a directory
+dirDocAux([File|FileTail], Path, Host, Html) ->
+    IsDir = filelib:is_dir(Path ++ File),
+    case IsDir of
+	 true -> %% File refers to a directory
 	    dirDocAux(FileTail, Path, Host, Html ++ "Dir:<A HREF='" ++ Path ++ "'>" ++ File ++ "</A>" ++ "<BR>");
-	isfile -> %% File refers to a file
-	    dirDocAux(FileTail, Path, Host, Html ++ "File:<A HREF='" ++ Path ++ "'>" ++ File ++ "</A>" ++ "<BR>");
-	Any  -> dirDocAux(FileTail, Path, Host, Html ++ "Error:<A HREF='" ++ Path ++ "'>" ++ File ++ "</A>" ++ "<BR>")
+	false -> %% File refers to a file
+	    dirDocAux(FileTail, Path, Host, Html ++ "File:<A HREF='" ++ Path ++ "'>" ++ File ++ "</A>" ++ "<BR>")
     end;
 dirDocAux([], _, _, Html) ->
     Html ++ "</BODY></HTML>".
