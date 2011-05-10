e0bf4fb7ce5e536d62c85d68ee9f83a180781ba9
DennisRosen <dennisrosen@ubuntu.ubuntu-domain>
2011-05-10 20:49:57 +0800

    fm fix

--- src/fm.erl
+++ src/fm.erl
@@ -7,25 +7,25 @@
 %% 			INTERNAL FUNCTIONS
 
 getContentType(FileName) ->
-    case string:sub_word(FileName, 2, $.) of
-	"jpg" ->
-	    "image/jpeg";
-	"txt" ->
-	    "text/plain";
-	"html" ->
-	    "text/html";
-	_ ->
-	    "text/plain"
-    end.
-
+	case string:sub_word(FileName, 2, $.) of
+		"jpg" ->
+			"image/jpeg";
+		"txt" ->
+			"text/plain";
+		"html" ->
+			"text/html";
+		_ ->
+			"text/plain"
+	end.
+	
 getCharset (FileName) ->
-    "UTF-8".
+	"UTF-8".
 
 getFileInfo(FileName) ->
-    [	{contenttype, getContentType(FileName)},
-	{charset, getCharset(FileName)}		].
-
-
+	[	{contenttype, getContentType(FileName)},
+		{charset, getCharset(FileName)}		].
+	
+	
 %%			EXPORTED FUNCTIONS
 
 %%		listDir(Directory)
@@ -34,11 +34,6 @@ getFileInfo(FileName) ->
 %%																																					files)
 
 dirHandler(Dir) ->
-<<<<<<< HEAD
-    case filelib:is_file(Dir) of
-	true ->
-	    case filelib:is_dir(Dir) of
-=======
 	case filelib:is_file(Dir) of
 		true ->
 			case filelib:is_dir(Dir) of
@@ -73,51 +68,21 @@ dirHandler(Dir) ->
 	
 fixPath (Path) ->
 	case filelib:is_dir(Path) of
->>>>>>> 2972afcd0c6848cc05fb6eaa32640962f5f34464
 		true ->
-		    case file:list_dir(Dir) of
-			{ok, DirList} ->
-			    DirListStripped = lists:filter(fun ([A | _]) -> A =/= $. end, DirList),	%% strip all ".*" files (eg hidden files)
-DirSort = fun(File1, File2) ->
-		  File1Dir = filelib:is_dir(Dir ++ File1),
-		  File2Dir = filelib:is_dir(Dir ++ File2),
-		  File1Low = string:to_lower(File1),
-		  File2Low = string:to_lower(File2),
-		  if
-		      File2Dir, File1Dir, File1Low > File2Low -> false;
-		      File2Dir == false, File1Dir == false, File1Low > File2Low -> false;
-		      File2Dir, File1Dir == false -> false;
-		      true -> true
-		  end
-	  end,
-{ok, lists:sort(DirSort, DirListStripped)};
-{error, Reason} ->
-    error_mod:handler(Reason)
-end;
-false ->
-    {error, eisfile}
-end;
-false ->
-    error_mod:handler(enotexist)
-end.
-
-fixPath(Path) ->
-    case filelib:is_dir(Path) of
-	true ->
-	    Path1 = case Path of	%% check for "/" at start of path
-			[$/ | _] ->	Path;
-			_		 ->
-			    [$/ | Path]
-		    end,
-	    case lists:last(Path1) of	%% check for "/" at end of path
-		$/ ->
-		    Path1;
-		_ ->
-		    Path1 ++ "/"
-	    end;
-	false ->
-	    Path
-    end.
+			Path1 = case Path of	%% check for "/" at start of path
+				[$/ | _] ->	Path;
+				_		 ->
+					[$/ | Path]
+			end,
+			case lists:last(Path1) of	%% check for "/" at end of path
+				$/ ->
+					Path1;
+				_ ->
+					Path1 ++ "/"
+			end;
+		false ->
+			Path
+	end.
 
 
 %% @spec (FileName::string()) -> {FileData, FileInfo}
@@ -136,17 +101,6 @@ fixPath(Path) ->
 %%  {charset,<string>}.
 
 getFile(FileName) -> 
-<<<<<<< HEAD
-    case file:read_file(FileName) of
-	{ok, Bin} ->
-	    {ok, {binary_to_list(Bin), getFileInfo(FileName)}};
-	{error, eisdir} ->
-	    {error, eisdir};
-	%%dirHandler(FileName);
-	{error, Reason} ->	%% {error, Reason}
-	    error_mod:handler(Reason)
-    end.
-=======
 	case file:read_file(FileName) of
 		{ok, Bin} ->
 			{ok, {binary_to_list(Bin), getFileInfo(FileName)}};
@@ -157,7 +111,6 @@ getFile(FileName) ->
 		{error, Reason} ->
 			error_mod:handler(Reason)
 	end.
->>>>>>> 2972afcd0c6848cc05fb6eaa32640962f5f34464
 
 
 %% 	 	getContents (FileHandle)
@@ -178,16 +131,16 @@ getInfoAll({_, InfoList}) -> InfoList.
 %% 	@doc	Returns the corresponding file info in filehandle FileHandle to the atom Info, or false if it is not found.
 %%			Example:	getInfo("hej.txt", contenttype)
 %% @spec (Filehandle()::{FileData, FileInfo}, List -> List
-
+	
 getInfo({_, InfoList}, Info) ->
-    case lists:keysearch(Info, 1, InfoList) of
-	{value, {_, Value}} ->
-	    Value;
-	false ->
-	    false
-    end.
-
-
+	case lists:keysearch(Info, 1, InfoList) of
+		{value, {_, Value}} ->
+			Value;
+		false ->
+			false
+	end.
+	
+	
 %%	Typical error reasons:
 %%
 %%	enoent
@@ -209,15 +162,12 @@ getInfo({_, InfoList}, Info) ->
 %% TEST CASES
 
 getInfo_test() ->
-    ?_assertMatch("text/html", getInfo({[], [{contenttype, "text/html"}]}, contenttype)).
-
+	?_assertMatch("text/html", getInfo({[], [{contenttype, "text/html"}]}, contenttype)).
+	
 getInfoAll_test() ->
-    ?_assertMatch([{contenttype, "text/html"}], getInfoAll({[], [{contenttype, "text/html"}]})).
-
+	?_assertMatch([{contenttype, "text/html"}], getInfoAll({[], [{contenttype, "text/html"}]})).
+	
 getContents_test() ->
-<<<<<<< HEAD
-    ?_assertMatch("hej jag ar en strang ur en fil", getContents({"hej jag ar en strang ur en fil", []})).
-=======
 	?_assertMatch("hej jag ar en strang ur en fil", getContents({"hej jag ar en strang ur en fil", []})).
 	
 fixPath_test() ->
@@ -235,4 +185,3 @@ fixPath_test() ->
 	
 	
 	
->>>>>>> 2972afcd0c6848cc05fb6eaa32640962f5f34464
