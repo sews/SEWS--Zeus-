b27b2d7da1fe3e6ae049756acc27e9c3e772c4ce
Kaakans <kaakans@gmail.com>
2011-05-12 08:00:35 -0700

    "Added Edoc, removed Host variable from dirDoc and dirDocAux as it was unused"

--- src/gen_html.erl
+++ src/gen_html.erl
@@ -1,43 +1,55 @@
+%% @version 0.2
+%% @doc A module made for generating html code
+%% @since 12.05.11
+
 -module(gen_html).
--compile(export_all).
+-compile([dirDoc/2]).
 
 -include_lib("eunit/include/eunit.hrl").
 
+%% //==================\\
+%% ||INTERNAL FUNCTIONS||
+%% \\==================//
 
-%% INTERNAL FUNCTIONS
+%% dirDocAux(DirList, Path, Html)
+%% @hidden@spec (DirList::list, Path::list, Html::list) -> String::list
+%% @hidden@doc Generates a simple UI to the file or directory at DirList.
+%%             The path to the current folder is contained at all points of execution
+%%             in the Path variable. The html code is saved in the variable Html.
+%%             Returns a string containing a html document.
+%% @hidden@private Removed Host as a variable
+%% @since 12.05.11
 
-%% @todo: Optimize
-dirDocAux(DirList, Path, Host, [])->
-    dirDocAux(DirList, Path, Host, "<html><head><title>Index of " ++ Path ++ "</title></head><body><h1>Index of " ++ Path ++ "</h1><hr>");
-dirDocAux([File|FileTail], Path, Host, Html) ->
+dirDocAux(DirList, Path, [])->
+    dirDocAux(DirList, Path, "<html><head><title>Index of " ++ Path ++ "</title></head><body><h1>Index of " ++ Path ++ "</h1><hr>");
+dirDocAux([File|FileTail], Path, Html) ->
     IsDir = filelib:is_dir(Path ++ File),
     case IsDir of
 	 true -> %% File refers to a directory
-	    dirDocAux(FileTail, Path, Host, Html ++ "[Dir] <a href='" ++ Path ++ File ++ "'>" ++ File ++ "</a>" ++ "<br />");
+	    dirDocAux(FileTail, Path, Html ++ "[Dir] <a href='" ++ Path ++ File ++ "'>" ++ File ++ "</a>" ++ "<br />");
 	false -> %% File refers to a file
-	    dirDocAux(FileTail, Path, Host, Html ++ "[File] <a href='" ++ Path ++ File ++ "'>" ++ File ++ "</a>" ++ "<br />")
+	    dirDocAux(FileTail, Path, Html ++ "[File] <a href='" ++ Path ++ File ++ "'>" ++ File ++ "</a>" ++ "<br />")
     end;
-dirDocAux([], _, _, Html) ->
+dirDocAux([], _, Html) ->
     Html ++ "<hr></body></html>".
     
+%% //==================\\
+%% ||EXPORTED FUNCTIONS||
+%% \\==================//
 
-%% EXPORTED FUNCTIONS
-
-%% @doc Returns a string containing HTML code to be used in a webserver, generated from files in DirList and headers in HList
+%% @doc Returns a string containing HTML code to be used in a webserver,
+%%      generated from files in DirList and headers in HList
 %% @spec dirDoc(DirList::list, Hlist::list) -> String::list
+%% @since 12.05.11
 
 dirDoc(DirList, HList)-> 
-    Path = case lists:keysearch(path, 1, HList) of
+
+%% Get path so requested file from HList
+    Path = case lists:keysearch(path, 1, HList) of 
     	{value,{path, P}} ->
 		   	P;
 	       	false ->
 		   	error_mod:handler(nopath)
-	   	end,
-    Host = case lists:keysearch(host, 1, HList) of	%% keysearch Path and Host for easy and fast access during HTML generation
-    	{value,{host, H}} ->
-			H;
-	       	false ->
-		   	error_mod:handler(nohost)
 	   	end,
     dirDocAux(DirList, Path, Host,[]).
     
