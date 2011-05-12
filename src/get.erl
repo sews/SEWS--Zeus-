2fc68b44884acf4d3ce921769ebc8edd8d75bb2b
Daniel Flodin <daniel-flodin@danielflodin-VirtualBox.(none)>
2011-05-12 21:02:55 +0200

    added edoc

--- src/get.erl
+++ src/get.erl
@@ -1,5 +1,6 @@
 %% @version 0.1
 %% @doc A module made for handling GET requests
+%% @since 2011-05-12
 
 -module(get).
 -export([handler/1]).
@@ -9,9 +10,12 @@
 %% //==================\\
 %% ||EXPORTED FUNCTIONS||
 %% \\==================//
-%% @spec ({atom(), Tuple_list}) -> Tuple_list
-%% @doc Takes a get-tuple with TupleList of GET headers, returns a TupleList with HTTP headers and a body.
+%% @spec ({atom, Tuplelist}) -> Tuplelist
+%% @doc Takes a get-tuple with a Tuplelist of different GET 
+%% 	headers and returns a TupleList with HTTP headers and a body.
+%% 	If the atom isn't 'get' an error gets returned
 %% @todo Implementera for more headers
+%% @since 2011-05-12 | 21:00
 
 handler({get, Tuple_list}) -> handlerAUX(Tuple_list);
 handler({_,_}) -> {error, notCorrectlyTagged}.
@@ -19,6 +23,16 @@ handler({_,_}) -> {error, notCorrectlyTagged}.
 %% //==================\\
 %% ||INTERNAL FUNCTIONS||
 %% \\==================//
+%% @spec (HList::Tuplelist) -> String | {error_eval, Bin} | {error, Reason}
+%% @doc Takes a Tuplelist with different headers and returns
+%% 	a string containing a file if the path-header corresponds 
+%%	to an existing file.
+%%	If the file does'nt exist it checks if the path-header refers
+%%	to a directory instead, if that's the case a list containing
+%%	all the files and directory's in the path-directory is returned.
+%%	If the path-header doesn't refer to neiher a file nor directory
+%%	an error is returned with information about why there was an error.
+%% @since 2011-05-12 | 21:00
 
 handlerAUX(HList) -> 
     case lists:keysearch(path, 1, HList) of
