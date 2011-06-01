3d257a5c3a578bb63a29671c69b1eb500b724308
DennisRosen <dennisrosen@ubuntu.ubuntu-domain>
2011-06-01 21:47:44 +0800

    latest sews version

--- src/main.erl
+++ src/main.erl
@@ -102,7 +102,7 @@ prepOSTProcessing (Parsed, Socket) ->
 handler(Socket) ->
     Data = case gen_tcp:recv(Socket, 0) of
 	       {ok, Indata} ->
-		   io:format("Request: ~n~p~n",[Indata]),
+		   %%io:format("Request: ~n~p~n",[Indata]),
 		   Parsed = parser:parse(binary_to_list(Indata)),
 		   case Parsed of
 		       {get, _} -> 
@@ -124,7 +124,7 @@ handler(Socket) ->
 		  Any ->
 		      Any
 	      end,
-    io:format("Answer: ~n~p~n",[Outdata]),
+    %%io:format("Answer: ~n~p~n",[Outdata]),
     gen_tcp:send(Socket, Outdata),
     gen_tcp:close(Socket).
 
